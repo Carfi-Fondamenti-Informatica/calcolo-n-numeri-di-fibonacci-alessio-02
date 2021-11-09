a10f6fbf2bb22d06c19e0fefc2be2b411c0bb6d5
alessio-02 <91601426+alessio-02@users.noreply.github.com>
2021-11-09 18:41:13 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-   return 0;
-}
+int fibonacci(int n) {
+    int a = 0, b = 1, c=0;
+    cout << 1 << endl;
+        for (int i = 2; i <= n; i++) {
+          c=a+b;
+          a=b;
+          b=c;
+          cout << c << endl;
+        }
+    }
+
+    int main() {
+        int n = 0;
+        cin >> n;
+        if (n < 2) {
+            cout << "errore" << endl;
+        } else {
+            fibonacci(n);
+        }
+        return 0;
+    }
